3bb1da72b1299ff2e6d9db24ace1783c1b6ffc10
Anders Berg <16289789+andersberg@users.noreply.github.com>
2024-06-07 15:30:07 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      hej
+    </React.StrictMode>
+  );
+}
