25433f10e6fd291631b2e30f93374a2d4e97abcb
Anders Berg <16289789+andersberg@users.noreply.github.com>
2024-07-19 10:15:07 +0200

    Initial backend commit

---
+++ backend/src/lib/ShoppingItem.ts
@@ -0,0 +1,23 @@
+import { nanoid } from "nanoid";
+import { z } from "zod";
+import { UNITS } from "./constants";
+
+export const ShoppingItemSchema = z.object({
+  id: z.string().nanoid().default(nanoid),
+  value: z.string().min(2, {
+    message: "Item must be at least 2 characters.",
+  }),
+  displayName: z.string().min(2, {
+    message: "Item must be at least 2 characters.",
+  }),
+  unit: z.enum(UNITS).default("st"),
+  quantity: z
+    .number()
+    .min(1, {
+      message: "Quantity must be at least 1.",
+    })
+    .default(1),
+  comment: z.string().optional(),
+});
+
+export type ShoppingItem = z.infer<typeof ShoppingItemSchema>;
