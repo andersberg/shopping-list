8f8ec78ac6336f9a8f29fa0bd8cf2ba04d6602cb
Anders Berg <16289789+andersberg@users.noreply.github.com>
2024-09-02 15:05:40 +0200

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.26.0
      project name = shopping-list-sveltekit
      framework = svelte
      framework cli = create-svelte@6.3.8
      package manager = bun@1.1.20
      wrangler = wrangler@3.73.0
      git = 2.45.2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,2 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-
-	console.log(data);
-</script>
-
-<p>{data.message}</p>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
